413b1baba575b3ca4886f5fb92246cf8af61cdf1
lans <wylans@163.com>
2019-06-19 17:46:00 +0800

    修复登录页面键盘遮挡报错

--- lib/login.dart
+++ lib/login.dart
@@ -42,95 +42,92 @@ class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
-        child: Container(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  "asset/ic_launcher.png",
-                  width: 80,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 100.0),
-                  child: Column(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 120, left: 20, right: 20),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              "asset/ic_launcher.png",
+              width: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 100.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "请输入手机号",
+                    ),
+                    autofocus: true,
+                    maxLength: 11,
+                  ),
+                  Stack(
+                    alignment: Alignment.centerRight,
                     children: <Widget>[
                       TextField(
                         keyboardType: TextInputType.number,
                         decoration: InputDecoration(
-                          hintText: "请输入手机号",
+                          hintText: "请输入验证码",
                         ),
-                        autofocus: true,
-                        maxLength: 13,
+                        maxLength: 6,
+                        onChanged: (data) {
+                          print("验证码$data");
+                        },
                       ),
-                      Stack(
-                        alignment: Alignment.centerRight,
-                        children: <Widget>[
-                          TextField(
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              hintText: "请输入验证码",
-                            ),
-                            maxLength: 6,
-                            onChanged: (data) {
-                              print("验证码$data");
-                            },
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: GestureDetector(
+                          onTap: () {
+                            if (countdownTime == 0) {
+                              startCountdown();
+                            }
+                          },
+                          child: Text(
+                            handleCodeText(),
+                            style: TextStyle(
+                                color: Color.fromRGBO(21, 201, 187, 1)),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 20),
-                            child: GestureDetector(
-                              onTap: () {
-                                if (countdownTime == 0) {
-                                  startCountdown();
-                                }
-                              },
-                              child: Text(
-                                handleCodeText(),
-                                style: TextStyle(
-                                    color: Color.fromRGBO(21, 201, 187, 1)),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                      )
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: MaterialButton(
-                    textColor: Colors.white,
-                    minWidth: window.physicalSize.width,
-                    color: Color.fromRGBO(21, 201, 187, 1),
-                    child: Text(
-                      "登录",
-                      style: TextStyle(fontSize: 13),
-                    ),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(60)),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) {
-                            return LoadingDialog(
-                              titleStyle: TextStyle(fontSize: 12),
-                            );
-                          });
-                      ///测试dialog消失
-                      Future(() {
-                        Future.delayed(Duration(seconds: 3), () {
-                          print("dialog消失了");
-                          Navigator.pop(context);
-                        });
-                      });
-                    },
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: MaterialButton(
+                textColor: Colors.white,
+                minWidth: window.physicalSize.width,
+                color: Color.fromRGBO(21, 201, 187, 1),
+                child: Text(
+                  "登录",
+                  style: TextStyle(fontSize: 13),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60)),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return LoadingDialog(
+                          titleStyle: TextStyle(fontSize: 12),
+                        );
+                      });
+
+                  ///测试dialog消失
+                  Future(() {
+                    Future.delayed(Duration(seconds: 3), () {
+                      print("dialog消失了");
+                      Navigator.pop(context);
+                    });
+                  });
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
